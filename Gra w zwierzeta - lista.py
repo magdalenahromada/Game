475dc624a3c1c9f44f62a70d6dba80341d71436e
Magdalena Hromada <magdalena.hromada@gmail.com>
2022-10-18 10:43:49 +0200

    Two commit

--- Gra w zwierzeta - lista.py
+++ Gra w zwierzeta - lista.py
@@ -10,6 +10,7 @@ import random
 zwierzeta = ["kot", "pies", "mysz", "królik", "panda", "wiewiórka", "biedronka", "żółw"]
 
 wylosowane = random.randint(0, 7)
+#My comment
 zwierze = zwierzeta[wylosowane]
 #print(zwierze) #wlasciwe
 zwierze_lista = list(zwierze)
@@ -23,6 +24,7 @@ print(zwierze_lista) #potasowane w liscie
 zwierze_uzytkownika = input("Hej, zgdanij co to za zwierze :")
 zwierze_uzytkownika_lista = list(zwierze_uzytkownika)
 while zwierze_uzytkownika_lista != przed_potasowaniem:
+#My comment
     print("Zgaduj dalej:")
     zwierze_uzytkownika = input()
     zwierze_uzytkownika_lista = list(zwierze_uzytkownika)
